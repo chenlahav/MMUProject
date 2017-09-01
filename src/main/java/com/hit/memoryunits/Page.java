06b307907a8cf52c14d45e8efec86bf671c874c5
chen lahav <chenlahav6@gmail.com>
2017-09-01 15:23:10 +0300

    add function hashcode, equals and toString in class Page

--- src/main/java/com/hit/memoryunits/Page.java
+++ src/main/java/com/hit/memoryunits/Page.java
@@ -24,19 +24,42 @@ public class Page<T>{
 	public void setPageId(Long id) {
 		this.id = id;
 	}
-	
-	//TODO
-	public int hashCode(){
-		return 1;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Page other = (Page) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Page [content=" + content + ", id=" + id + "]";
 	}
 	
-	//TODO
-	public boolean equals(Object obj){
-		return false;
-	}
 	
-	//TODO
-	public String toString(){
-		return this.getPageId().toString() + this.getContent().toString();
-	}
 }
