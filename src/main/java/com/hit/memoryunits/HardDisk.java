9137cfa2617c4046e539a2a6e1ef2c0560f9d5bb
chen lahav <chenlahav6@gmail.com>
2017-09-02 13:47:16 +0300

    singletone

--- src/main/java/com/hit/memoryunits/HardDisk.java
+++ src/main/java/com/hit/memoryunits/HardDisk.java
@@ -1,12 +1,22 @@
 package com.hit.memoryunits;
-import java.lang.*;
 import java.io.*;
 
 public class HardDisk {
+	private int _SIZE;
+	private String DEFAULT_FILE_NAME;
+	private static HardDisk instance = null;
+	
+	private HardDisk(int _SIZE, String DEFAULT_FILE_NAME) {
+		this._SIZE = _SIZE;
+		this.DEFAULT_FILE_NAME = DEFAULT_FILE_NAME;
+	}
 		
 	//TODO
 	public static HardDisk getInstance(){
-		return null;
+		if(instance==null){
+			instance=new HardDisk(1000,"/filename.txt");
+		}
+		return instance;
 	}
 	
 	//TODO
